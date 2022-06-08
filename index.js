392e29e85f24ccce7cfbbde66c48bee61fe472df
gmluqa <gmluqa@protonmail.com>
2022-06-08 12:15:46 +0200

    Added bones of fund()

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+// raw JS uses import instead of require
+
 //https://docs.metamask.io/guide/
 async function connect() {
-  if (typeof window.ethereum !== "undefined") {
-    //connection script
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-    document.getElementById("connectButton").innerHTML = "Connected!";
-  } else {
-    document.getElementById("connectButton").innerHTML =
-      "Please install metamask!";
-  }
+    if (typeof window.ethereum !== "undefined") {
+        //connection script
+        await window.ethereum.request({ method: "eth_requestAccounts" })
+        document.getElementById("connectButton").innerHTML = "Connected!"
+    } else {
+        document.getElementById("connectButton").innerHTML =
+            "Please install metamask!"
+    }
 }
+
+async function fund(ethAmount) {
+    console.log("Funding with " + ethAmount + "...")
+}
+
+// withdraw function
