bd722922890d87b9256d4dad63c7b455833f58cf
gmluqa <gmluqa@protonmail.com>
2022-06-08 12:40:12 +0200

    imported {ethers}

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // raw JS uses import instead of require
+import { ethers } from "./ethers-5.6.esm.min.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+// .onclick is all lowercase, no ()
+connectButton.onclick = connect
+fundButton.onclick = fund
 
 //https://docs.metamask.io/guide/
 async function connect() {
@@ -14,6 +21,12 @@ async function connect() {
 
 async function fund(ethAmount) {
     console.log("Funding with " + ethAmount + "...")
+    if (typeof window.ethereum !== "undefined") {
+        //need api to blockchain
+        //need signer/wallet
+        //need contract to interact with
+        //need ABI + interface
+    }
 }
 
 // withdraw function
